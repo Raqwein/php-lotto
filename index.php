0928db7bb735be325869e9326f56eb9506c5b3a7
diak <diak@10.9.8.20>
2019-02-19 07:49:49 +0100

    szelvény megjelenítése

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Lottó</title>
     </head>
     <body>
         Ez az index.php
@@ -11,9 +11,36 @@
         $sorsolt=sorsolas();
         var_dump ($sorsolt);
             //lottohúzás
- 
-        echo "Ezek voltak a számok";
-        echo "Itt már a php script fut";
+        $szamokszama=90;
+        $oszlop=15;
+        echo '<table border="1">';
+	
+	for($i=1; $i<=$szamokszama; $i++)
+	{
+		if($i%$oszlop==1)
+		{
+			echo "<tr>";
+		}
+		if(isset($sorsolt[$i]))
+		{
+			$hatterszin="#FF0000";
+		}
+		else
+		{
+			$hatterszin="#FFFFFF";
+		}
+		echo "<td bgcolor=\"$hatterszin\" >";
+			echo $i;
+		echo "</td>";
+		if($i%$oszlop==0)
+		{
+			echo "</tr>\n";
+		}
+	}
+	
+	echo "</table>";
+        //echo "Ezek voltak a számok";
+        //echo "Itt már a php script fut";
         ?>
     </body>
 </html>
